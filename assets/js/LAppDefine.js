e2de0430e06fb88e47f52bd1fb276e3436c9af00
ylia233 <ylia233@163.com>
2017-08-13 17:55:59 +0000

    first commit

---
+++ assets/js/LAppDefine.js
@@ -0,0 +1,44 @@
+var LAppDefine = {
+    
+    
+    DEBUG_LOG : true,
+    DEBUG_MOUSE_LOG : false, 
+    // DEBUG_DRAW_HIT_AREA : true, 
+    // DEBUG_DRAW_ALPHA_MODEL : false, 
+    
+    
+    
+    
+    VIEW_MAX_SCALE : 2,
+    VIEW_MIN_SCALE : 0.8,
+
+    VIEW_LOGICAL_LEFT : -1,
+    VIEW_LOGICAL_RIGHT : 1,
+
+    VIEW_LOGICAL_MAX_LEFT : -2,
+    VIEW_LOGICAL_MAX_RIGHT : 2,
+    VIEW_LOGICAL_MAX_BOTTOM : -2,
+    VIEW_LOGICAL_MAX_TOP : 2,
+    
+    
+    PRIORITY_NONE : 0,
+    PRIORITY_IDLE : 1,
+    PRIORITY_NORMAL : 2,
+    PRIORITY_FORCE : 3,
+    
+
+    BACK_IMAGE_NAME : "assets/image/background.png",
+
+    MODEL_YLIA: "assets/live2d/ylia02/ylia02.model.json",
+    MOTION_GROUP_IDLE : "idle", 
+    MOTION_GROUP_TAP_BODY : "tap_body",
+    MOTION_GROUP_TAP_BREAST : "tap_breast",
+    MOTION_GROUP_TAP_FACE : "tap_face",
+    MOTION_GROUP_TAP_HEAD : "tap_head",
+    MOTION_GROUP_TAP_HELLO : "tap_hello",
+
+    HIT_AREA_BODY : "body",
+    HIT_AREA_BREAST : "breast",
+    HIT_AREA_FACE : "face",
+    HIT_AREA_HEAD : "head"
+};
